8dd361a43abeaf73954f78810195e1d75f6597f6
Champana Julian Bernard <champana.bernard.18@cnu.edu>
2020-02-18 22:13:22 -0500

    proj 2

--- src/array_functions.cpp
+++ src/array_functions.cpp
@@ -7,18 +7,46 @@
 
 //============================================================================
 //	TODO add necessary includes here
-//============================================================================
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <time.h>
 
+
+#include "array_functions.h"
+#include "constants.h"
+#include "Project2.cpp"
+#include "utilities.cpp"
+#include "utilities.h"
+//============================================================================
+using namespace std;
 //============================================================================
 //	stuff you will need
 //============================================================================
 //TODO define a structure to track words and number of times they occur
-
+struct entry {
+	word
+	upper
+	count
+};
 //TODO add a global array of entry structs (global to this file)
-
+int count[max]
 //TODO add variable to keep track of next available slot in array
-
+for(i) in entry{
+	
+	
+};
 //TODO define all functions in header file
 
 //TODO look in utilities.h for useful functions, particularly strip_unwanted_chars!
+#ifndef UTILITIES_H_
+#define UTILITIES_H_
+
+	void toUpper(std::string &myString);
+	std::string intToString(int Number); 
+	void strip_char(std::string &s, char badchar);
+	bool strip_unwanted_chars(std::string &mystring);
+
+#endif /* UTILITIES_H_ */
 
